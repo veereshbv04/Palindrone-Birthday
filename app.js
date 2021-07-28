fadcb3324e35ba574e4901261db5634a58082713
Veeresh B V <Veereshbv04@gmail.com>
2021-07-28 15:16:35 +0530

    button is working

---
+++ app.js
@@ -0,0 +1,16 @@
+const userDate = document.querySelector("#userbirthdate");
+const submit = document.querySelector("#submit");
+const maindiv = document.querySelector("#main-div");
+console.log("jj");
+
+
+
+submit.addEventListener("click",()=>{
+
+
+
+    console.log("Submite button pressed");
+
+
+  
+})
