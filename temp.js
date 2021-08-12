7bd36ae06cde426c828ed92c865939585c9b16c6
Veeresh B V <Veereshbv04@gmail.com>
2021-08-13 00:50:34 +0530

    nextdate is working

--- temp.js
+++ temp.js
@@ -13,9 +13,9 @@ function isPalindrome(str){
 }
 
 var date = {
-    day :13,
-    month :9,
-    year : 2011
+    day :31,
+    month :12,
+    year : 2020
 };
 
 function convertDateToString(date){
@@ -79,4 +79,67 @@ function checkPalindromeForAllDateFormats(date){
     return flag;
 }
 
-console.log(checkPalindromeForAllDateFormats(date))
+function isLeapYear(year){
+    if(year%400 === 0){
+        return true;
+    }
+    if(year % 100 === 0){
+        return false;
+    }
+    if(year%4 === 0){
+        return true;
+    }
+
+    return false;
+
+}
+
+
+
+function getNextDate(date){
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30,31,30,31,31,30,31,30,31];
+
+    if(month === 2){
+        // check for leap year
+        if(isLeapYear(year)){
+            if(day > 29){
+                day =1;
+                month = month +1;
+            }
+        }
+        else{
+            if(day > 28){
+                day = 1;
+                month = month + 1;
+            }
+        }
+    }else{
+        if(day> daysInMonth[month - 1]){
+            day =1;
+            month =month+1;
+        }
+    }
+
+    if(month>12){
+        month =1;
+        year = year +1;
+    }
+
+    return {
+        day : day,
+        month : month,
+        year : year
+    };
+
+
+}
+
+console.log(getNextDate(date))
+
+function getNextPalindromeDate(date){
+
+}
